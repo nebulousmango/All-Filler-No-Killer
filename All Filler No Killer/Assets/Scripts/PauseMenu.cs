e7c6a73d4e15934269d1e5311a2735501f41e9d5
nebulousmango <sitaradaniel@gmail.com>
2024-05-19 13:54:23 +0530

    UI scenes

--- All Filler No Killer/Assets/Scripts/PauseMenu.cs
+++ All Filler No Killer/Assets/Scripts/PauseMenu.cs
@@ -3,12 +3,14 @@ using UnityEngine;
 public class PauseMenu : MonoBehaviour
 {
     [SerializeField] private GameObject Go_PauseMenuUI;
+    [SerializeField] private GameObject Go_ControlsUI;
     [SerializeField] private GameObject Go_OverlayUI;
     static bool GameIsPaused = false;
 
     private void Start()
     {
         Go_PauseMenuUI.SetActive(false);
+        Go_ControlsUI.SetActive(false);
         Go_OverlayUI.SetActive(true);
     }
 
@@ -29,6 +31,7 @@ public class PauseMenu : MonoBehaviour
     public void Pause()
     {
         Go_PauseMenuUI.SetActive(true);
+        Go_ControlsUI.SetActive(false);
         Go_OverlayUI.SetActive(false);
         Time.timeScale = 0;
         GameIsPaused = true;
@@ -37,9 +40,15 @@ public class PauseMenu : MonoBehaviour
     void Resume()
     {
         Go_PauseMenuUI.SetActive(false);
+        Go_ControlsUI.SetActive(false);
         Go_OverlayUI.SetActive(true);
         GameIsPaused = false;
         Time.timeScale = 1;
+    }
+
+    public void Controls()
+    {
+        Go_ControlsUI.SetActive(true);
     }
 
     public void ChangePauseBool()
