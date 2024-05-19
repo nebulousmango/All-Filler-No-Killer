2848fe249e76ac1a16c2911c44df8e4ba267af48
nebulousmango <sitaradaniel@gmail.com>
2024-05-19 13:11:08 +0530

    UI update

---
+++ All Filler No Killer/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    [SerializeField] private string S_LevelA;
+    [SerializeField] private string S_LevelB;
+    [SerializeField] private string S_LevelC;
+    [SerializeField] private string S_LevelD;
+
+    public void LoadLevelA()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(S_LevelA);
+    }
+
+    public void LoadLevelB()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(S_LevelB);
+    }
+
+    public void LoadLevelC()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(S_LevelC);
+    }
+    public void LoadLevelD()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(S_LevelD);
+    }
+
+    public void RestartLevel()
+    {
+        Time.timeScale = 1;
+        FindObjectOfType<PauseMenu>().ChangePauseBool();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ All Filler No Killer/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject Go_PauseMenuUI;
+    [SerializeField] private GameObject Go_OverlayUI;
+    static bool GameIsPaused = false;
+
+    private void Start()
+    {
+        Go_PauseMenuUI.SetActive(false);
+        Go_OverlayUI.SetActive(true);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    public void Pause()
+    {
+        Go_PauseMenuUI.SetActive(true);
+        Go_OverlayUI.SetActive(false);
+        Time.timeScale = 0;
+        GameIsPaused = true;
+    }
+
+    void Resume()
+    {
+        Go_PauseMenuUI.SetActive(false);
+        Go_OverlayUI.SetActive(true);
+        GameIsPaused = false;
+        Time.timeScale = 1;
+    }
+
+    public void ChangePauseBool()
+    {
+        GameIsPaused = false;
+    }
+}
